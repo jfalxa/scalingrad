765877ccada46698b97416cbc64dbeef81cb7ed2
jfalxa <jfalxa.sta@gmail.com>
2022-06-20 15:49:53 +0200

    try to fix config

--- api/core/settings.py
+++ api/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env.get("SECRET_KEY")
 
 DEBUG = env.get("DEBUG") == "1"
 
-ALLOWED_HOSTS = env.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.get("ALLOWED_HOSTS", "").split(",")
 
 STATIC_URL = "/static/"
 STATIC_ROOT = path.join(BASE_DIR, "static")

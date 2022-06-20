82beeccc0780a62445294b950d8cc9834bb45656
jfalxa <jfalxa.sta@gmail.com>
2022-06-20 16:17:57 +0200

    try to fix static file serving

--- api/core/settings.py
+++ api/core/settings.py
@@ -17,6 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -109,8 +110,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = "static"
+STATIC_URL = "/static/"
 
 
 # Default primary key field type
